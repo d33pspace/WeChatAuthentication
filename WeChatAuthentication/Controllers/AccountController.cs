5ec8eeb51bd472e8a19dd678a7ef3ebff83a5eee
meteoric9637 <meteoric_stone@hotmail.com>
2017-07-17 10:34:09 +0800

    change account controller.

--- WeChatAuthentication/Controllers/AccountController.cs
+++ WeChatAuthentication/Controllers/AccountController.cs
@@ -123,7 +123,7 @@ namespace WeChatAuthentication.Controllers
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     //var callbackUrl = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-                    //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
+                    //    $"Please confirm your account by clicking tadafhis link: <a href='{callbackUrl}'>link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
                     return RedirectToLocal(returnUrl);
